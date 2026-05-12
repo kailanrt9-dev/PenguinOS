cb8729cf9d15d89144602006c08af9d84fa941ed
kailanrt9-dev <kailanrt9@gmail.com>
2026-05-12 07:54:05 -0500

    push welcome.sh to the OS project

---
+++ .github/init/welcome.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# PenguinOS 1.0 Boot Script
+
+clear
+echo "          _P_"
+echo "         /   \\"
+echo "        | P P |"
+echo "        |  _  |"
+echo "        /  _  \\"
+echo "       / /   \\ \\"
+echo "      (_|     |_)"
+echo ""
+echo "Welcome to PenguinOS 1.0, Kailan!"
+echo "System initialized. Cloud build successful."
+echo ""
+/bin/sh
